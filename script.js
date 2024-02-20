61e68b429a9bbf444afad94c99a591aee77f4fe9
GrimDevxlx <slj1999x@gmail.com>
2024-02-20 13:10:00 +0800

    update
    scripsss

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ let addToDoButton = document.getElementById('addToDo');
 let ToDoContainer = document.getElementById('ToDoContainer');
 let inputField = document.getElementById('inputField');
 
-  
+addToDoButton.addEventListener('click', function(){
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  ToDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener('click', function(){
+      paragraph.style.textDecoration = "line-through";
+  })
+  paragraph.addEventListener('dblclick', function(){
+      ToDoContainer.removeChild(paragraph);
+  })
+})
